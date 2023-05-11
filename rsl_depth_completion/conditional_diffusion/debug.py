cea7e307a62a347fe3918541a0fac6c6959a8415
kirilllzaitsev <kzaitsev88@gmail.com>
2023-05-11 21:21:50 +0200

    set best ds_kwargs based on experiments

--- rsl_depth_completion/conditional_diffusion/debug.py
+++ rsl_depth_completion/conditional_diffusion/debug.py
@@ -44,103 +44,112 @@ else:
 
 # shutil.rmtree(logdir, ignore_errors=True)
 
+ds_name = "mnist"
+best_params = {
+    "kitti": {
+        "use_text_embed": False,
+        "use_cond_image": True,
+        "use_rgb_as_cond_image": False,
+    },
+    "mnist": {
+        "use_text_embed": True,
+        "use_cond_image": False,
+        "use_rgb_as_cond_image": True,
+    },
+}
 
-# for ds_name in ["mnist", "kitti"]:
-for ds_name in ["mnist"]:
-    for ds_kwargs in [next(ds_params)]:
-        # for ds_kwargs in ds_params:
-        ds_kwargs["use_rgb_as_text_embed"] = not ds_kwargs["use_rgb_as_cond_image"]
-        ds_kwargs["include_sdm_and_rgb_in_sample"] = True
-        ds_kwargs["do_crop"] = True
-        print(ds_kwargs)
+ds_kwargs = best_params[ds_name]
 
-        ds, train_dataloader, val_dataloader = load_data(
-            ds_name=ds_name, do_overfit=cfg.do_overfit, **ds_kwargs
-        )
+ds_kwargs["use_rgb_as_text_embed"] = not ds_kwargs["use_rgb_as_cond_image"]
+ds_kwargs["include_sdm_and_rgb_in_sample"] = True
+ds_kwargs["do_crop"] = True
+print(ds_kwargs)
 
-        experiment = comet_ml.Experiment(
-            api_key="W5npcWDiWeNPoB2OYkQvwQD0C",
-            project_name="rsl_depth_completion",
-            auto_metric_logging=True,
-            auto_param_logging=True,
-            auto_histogram_tensorboard_logging=True,
-            log_env_details=False,
-            log_env_host=False,
-            disabled=cfg.disabled,
-        )
-        log_params_to_exp(experiment, ds_kwargs, "dataset")
-        log_params_to_exp(experiment, cfg.params(), "base_config")
+ds, train_dataloader, val_dataloader = load_data(
+    ds_name=ds_name, do_overfit=cfg.do_overfit, **ds_kwargs
+)
 
-        print(
-            "Number of train samples",
-            len(train_dataloader) * train_dataloader.batch_size,
-        )
+experiment = comet_ml.Experiment(
+    api_key="W5npcWDiWeNPoB2OYkQvwQD0C",
+    project_name="rsl_depth_completion",
+    auto_metric_logging=True,
+    auto_param_logging=True,
+    auto_histogram_tensorboard_logging=True,
+    log_env_details=True,
+    log_env_host=False,
+    log_env_gpu=True,
+    log_env_cpu=True,
+    disabled=cfg.disabled,
+)
+log_params_to_exp(experiment, ds_kwargs, "dataset")
+log_params_to_exp(experiment, cfg.params(), "base_config")
 
-        import gc
+print(
+    "Number of train samples",
+    len(train_dataloader) * train_dataloader.batch_size,
+)
 
-        from model import init_model
+import gc
 
-        gc.collect()
-        torch.cuda.empty_cache()
+from model import init_model
 
-        unets, imagen = init_model(cfg.timesteps, experiment, ds_kwargs)
+gc.collect()
+torch.cuda.empty_cache()
 
-        unets[0].to(cfg.device)
-        imagen.to(cfg.device)
+unets, imagen = init_model(cfg.timesteps, experiment, ds_kwargs)
 
-        print(
-            "Number of parameters in model",
-            sum(p.numel() for p in imagen.parameters() if p.requires_grad),
-        )
+unets[0].to(cfg.device)
+imagen.to(cfg.device)
 
-        optimizer = optim.Adam(imagen.parameters(), lr=cfg.lr)
+print(
+    "Number of parameters in model",
+    sum(p.numel() for p in imagen.parameters() if p.requires_grad),
+)
 
-        input_name = "interp_sdm"
+optimizer = optim.Adam(imagen.parameters(), lr=cfg.lr)
 
-        if ds_kwargs["use_cond_image"]:
-            if ds_kwargs["use_rgb_as_cond_image"]:
-                img_cond = "rgb"
-            else:
-                img_cond = "sdm"
-        else:
-            img_cond = "none"
+input_name = "interp_sdm"
 
-        if ds_kwargs["use_text_embed"]:
-            if ds_kwargs["use_rgb_as_text_embed"]:
-                text_cond = "rgb"
-            else:
-                text_cond = "sdm"
-        else:
-            text_cond = "none"
+if ds_kwargs["use_cond_image"]:
+    if ds_kwargs["use_rgb_as_cond_image"]:
+        img_cond = "rgb"
+    else:
+        img_cond = "sdm"
+else:
+    img_cond = "none"
 
-        cond = f"{img_cond=}_{text_cond=}"
-        exp_dir = f"{input_name=}/{cond=}/{cfg.lr=}_{cfg.timesteps=}"
+if ds_kwargs["use_text_embed"]:
+    if ds_kwargs["use_rgb_as_text_embed"]:
+        text_cond = "rgb"
+    else:
+        text_cond = "sdm"
+else:
+    text_cond = "none"
 
-        exp_dir = (
-            f"{len(os.listdir(logdir)) + 1:03d}" if os.path.isdir(logdir) else "001"
-        )
-        train_logdir = logdir / exp_dir / cond
-        train_logdir.mkdir(parents=True, exist_ok=True)
-        train_writer = tf.summary.create_file_writer(str(train_logdir))
-        with train_writer.as_default():
-            tf.summary.text(
-                "hyperparams", dict2mdtable({**ds_kwargs, **cfg.params()}), 1
-            )
+cond = f"{img_cond=}_{text_cond=}"
+exp_dir = f"{input_name=}/{cond=}/{cfg.lr=}_{cfg.timesteps=}"
 
-        from train import train
+exp_dir = f"{len(os.listdir(logdir)) + 1:03d}" if os.path.isdir(logdir) else "001"
+train_logdir = logdir / exp_dir / cond
+train_logdir.mkdir(parents=True, exist_ok=True)
+train_writer = tf.summary.create_file_writer(str(train_logdir))
+with train_writer.as_default():
+    tf.summary.text("hyperparams", dict2mdtable({**ds_kwargs, **cfg.params()}), 1)
 
-        train(
-            imagen,
-            optimizer,
-            train_dataloader,
-            out_dir=train_logdir,
-            train_writer=train_writer,
-        )
+from train import train
 
-        experiment.add_tags([k for k, v in ds_kwargs.items() if v])
-        experiment.add_tags(cfg.other_tags)
-        experiment.add_tag(ds_name)
-        experiment.add_tag("overfit" if cfg.do_overfit else "full_data")
-        # experiment.add_tag("debug" if cfg.do_debug else "train")
+train(
+    imagen,
+    optimizer,
+    train_dataloader,
+    out_dir=train_logdir,
+    train_writer=train_writer,
+)
 
-        experiment.end()
+experiment.add_tags([k for k, v in ds_kwargs.items() if v])
+experiment.add_tags(cfg.other_tags)
+experiment.add_tag(ds_name)
+experiment.add_tag("overfit" if cfg.do_overfit else "full_data")
+# experiment.add_tag("debug" if cfg.do_debug else "train")
+
+experiment.end()
