18a4e0534f335e3714b4a7b8d318f9b8fb96ec15
kirilllzaitsev <kzaitsev88@gmail.com>
2023-06-04 16:31:55 +0200

    feat: add resize, fix_channels

--- rsl_depth_completion/conditional_diffusion/img_utils.py
+++ rsl_depth_completion/conditional_diffusion/img_utils.py
@@ -1,12 +1,6 @@
-import os
-from glob import glob
-from pathlib import Path
 
-import cv2
-import matplotlib.pyplot as plt
 import numpy as np
-import torch
-from PIL import Image
+import torchvision as tv
 
 
 def center_crop(img, crop_size, channels_last=False):
@@ -20,3 +14,42 @@ def center_crop(img, crop_size, channels_last=False):
     if channels_last:
         return img[i : i + th, j : j + tw, :]
     return img[:, i : i + th, j : j + tw]
+
+
+def resize(
+    img: np.ndarray, size, interpolation=tv.transforms.InterpolationMode.BICUBIC
+):
+    assert interpolation in [
+        tv.transforms.InterpolationMode.BICUBIC,
+        tv.transforms.InterpolationMode.BILINEAR,
+    ]
+    transform = tv.transforms.Compose(
+        [
+            tv.transforms.Resize(size, antialias=True, interpolation=interpolation),
+        ]
+    )
+    img = fix_channel_not_first(img)
+    resized = transform(img)
+    return resized
+
+
+def fix_channel_not_last(x):
+    assert len(x.shape) >= 3, "Must be at least a 3D tensor"
+    if len(x.shape) == 3:
+        if x.shape[0] < x.shape[-1]:
+            return np.transpose(x, (1, 2, 0))
+    elif len(x.shape) == 4:
+        if x.shape[1] < x.shape[-1]:
+            return np.transpose(x, (0, 2, 3, 1))
+    return x
+
+
+def fix_channel_not_first(x):
+    assert len(x.shape) >= 3, "Must be at least a 3D tensor"
+    if len(x.shape) == 3:
+        if x.shape[0] > x.shape[-1]:
+            return np.transpose(x, (2, 0, 1))
+    elif len(x.shape) == 4:
+        if x.shape[1] > x.shape[-1]:
+            return np.transpose(x, (0, 3, 1, 2))
+    return x
