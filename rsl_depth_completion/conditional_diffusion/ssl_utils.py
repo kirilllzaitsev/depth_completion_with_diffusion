7786774caf4d66e4707936089af48a28c3cbdf09
kirilllzaitsev <kzaitsev88@gmail.com>
2023-06-13 17:35:30 +0200

    move triplet loss to ssl_utils

--- rsl_depth_completion/conditional_diffusion/ssl_utils.py
+++ rsl_depth_completion/conditional_diffusion/ssl_utils.py
@@ -1,6 +1,10 @@
+import os
+import cv2
+
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+from kbnet import log_utils, losses, net_utils, networks
 from rsl_depth_completion.models.benchmarking.calibrated_backprojection_network.kbnet import (
     eval_utils,
 )
@@ -9,13 +13,15 @@ from rsl_depth_completion.models.benchmarking.calibrated_backprojection_network.
 def plot_losses(losses, rec_losses):
     import pandas as pd
 
-    fig, axs = plt.subplots(1, len(losses), figsize=(20, 5))
+    fig, axs = plt.subplots(1, len(losses), figsize=(25, 5))
     fig.suptitle("Main losses")
     for i, (k, v) in enumerate(losses.items()):
         axs[i].plot(v)
         axs[i].set_title(k)
+        axs[i].set_xlabel("Step")
+        axs[i].set_ylabel("Loss")
     plt.tight_layout()
-    plt.subplots_adjust(wspace=0.3)
+    plt.subplots_adjust(wspace=0.7)
     plt.show()
     pd.DataFrame(rec_losses).plot(title="Reconstruction losses", figsize=(4, 2))
 
@@ -89,11 +95,162 @@ def plot_grads(depth_grads, pose_grads, topk=5, bottomk=5):
             # if np.isnan(v).any():
             #     print(f"NaN in {k}. {title} invalid.")
             #     return
+        ax.set_xlabel("Step")
+        ax.set_ylabel("L1 norm")
         try:
-            pd.DataFrame({k: v for k, v in grads.items() if v}).plot(title=title, ax=ax)
+            pd.DataFrame({k: v for k, v in grads.items() if v}).plot(title=title, ax=ax, legend=False)
         except:
             ax.set_title(f"{title} (empty)")
 
-    display_grads(depth_grads, axs[0], "Depth gradients")
-    display_grads(pose_grads, axs[1], "Pose gradients")
+    display_grads(depth_grads, axs[0], "Depth model gradients")
+    display_grads(pose_grads, axs[1], "Pose model gradients")
     plt.show()
+    return fig
+
+
+def plot_train_depths_overall(output_depths, save_dir=None, save_subdir_prefix=""):
+    figs = []
+    batch_size = output_depths[0]
+    for idx in range(len(batch_size)):
+        pred_ckpts = np.linspace(
+            0, len(output_depths) - 1, min(20, len(output_depths))
+        ).astype(int)
+        n_rows = len(pred_ckpts) // 5 if len(pred_ckpts) % 5 == 0 else len(pred_ckpts) // 5 + 1
+        n_cols = min(5, len(pred_ckpts))
+        fig, axs = plt.subplots(n_rows, n_cols, figsize=(20, 10))
+        for i, ckpt in enumerate(pred_ckpts):
+            pred = output_depths[ckpt][idx].detach().cpu().numpy().squeeze()
+            if n_rows == 1:
+                if n_cols == 1:
+                    ax = axs
+                else:
+                    ax = axs[i % 5]
+            else:
+                ax = axs[i // 5, i % 5]
+            ax.imshow(pred)
+            ax.set_title(f"Epoch {ckpt}")
+            ax.axis("off")
+            if save_dir is not None:
+                dst_dir = os.path.join(save_dir, f"{save_subdir_prefix}/{idx}")
+                os.makedirs(dst_dir, exist_ok=True)
+                filename = f"epoch_{ckpt}.png"
+                cv2.imwrite(os.path.join(dst_dir, filename), pred)
+                # plt.savefig(os.path.join(dst_dir, filename))
+        plt.tight_layout()
+        figs.append(fig)
+    return figs
+
+
+def compute_triplet_loss(
+    image0,
+    image1,
+    image2,
+    output_depth0,
+    sparse_depth0,
+    validity_map_depth0,
+    intrinsics,
+    pose01,
+    pose02,
+    w_color=0.15,
+    w_structure=0.95,
+    w_sparse_depth=0.60,
+    w_smoothness=0.04,
+):
+    """
+    Computes loss function
+    l = w_{ph}l_{ph} + w_{sz}l_{sz} + w_{sm}l_{sm}
+
+    Arg(s):
+        image0 : torch.Tensor[float32]
+            N x 3 x H x W image at time step t
+        image1 : torch.Tensor[float32]
+            N x 3 x H x W image at time step t-1
+        image2 : torch.Tensor[float32]
+            N x 3 x H x W image at time step t+1
+        output_depth0 : torch.Tensor[float32]
+            N x 1 x H x W output depth at time t
+        sparse_depth0 : torch.Tensor[float32]
+            N x 1 x H x W sparse depth at time t
+        validity_map_depth0 : torch.Tensor[float32]
+            N x 1 x H x W validity map of sparse depth at time t
+        intrinsics : torch.Tensor[float32]
+            N x 3 x 3 camera intrinsics matrix
+        pose01 : torch.Tensor[float32]
+            N x 4 x 4 relative pose from image at time t to t-1
+        pose02 : torch.Tensor[float32]
+            N x 4 x 4 relative pose from image at time t to t+1
+        w_color : float
+            weight of color consistency term
+        w_structure : float
+            weight of structure consistency term (SSIM)
+        w_sparse_depth : float
+            weight of sparse depth consistency term
+        w_smoothness : float
+            weight of local smoothness term
+    Returns:
+        torch.Tensor[float32] : loss
+        dict[str, torch.Tensor[float32]] : dictionary of loss related tensors
+    """
+
+    shape = image0.shape
+    validity_map_image0 = torch.ones_like(sparse_depth0)
+
+    # Backproject points to 3D camera coordinates
+    points = net_utils.backproject_to_camera(output_depth0, intrinsics, shape)
+
+    # Reproject points onto image 1 and image 2
+    target_xy01 = net_utils.project_to_pixel(points, pose01, intrinsics, shape)
+    target_xy02 = net_utils.project_to_pixel(points, pose02, intrinsics, shape)
+
+    # Reconstruct image0 from image1 and image2 by reprojection
+    image01 = net_utils.grid_sample(image1, target_xy01, shape)
+    image02 = net_utils.grid_sample(image2, target_xy02, shape)
+
+    """
+    Essential loss terms
+    """
+    # Color consistency loss function
+    loss_color01 = losses.color_consistency_loss_func(
+        src=image01, tgt=image0, w=validity_map_image0
+    )
+    loss_color02 = losses.color_consistency_loss_func(
+        src=image02, tgt=image0, w=validity_map_image0
+    )
+    loss_color = loss_color01 + loss_color02
+
+    # Structural consistency loss function
+    loss_structure01 = losses.structural_consistency_loss_func(
+        src=image01, tgt=image0, w=validity_map_image0
+    )
+    loss_structure02 = losses.structural_consistency_loss_func(
+        src=image02, tgt=image0, w=validity_map_image0
+    )
+    loss_structure = loss_structure01 + loss_structure02
+
+    # Sparse depth consistency loss function
+    loss_sparse_depth = losses.sparse_depth_consistency_loss_func(
+        src=output_depth0, tgt=sparse_depth0, w=validity_map_depth0
+    )
+
+    # Local smoothness loss function
+    loss_smoothness = losses.smoothness_loss_func(predict=output_depth0, image=image0)
+
+    # l = w_{ph}l_{ph} + w_{sz}l_{sz} + w_{sm}l_{sm}
+    loss = (
+        w_color * loss_color +
+        w_structure * loss_structure
+        + w_sparse_depth * loss_sparse_depth
+        + w_smoothness * loss_smoothness
+    )
+
+    loss_info = {
+        "loss_color": loss_color,
+        "loss_structure": loss_structure,
+        "loss_sparse_depth": loss_sparse_depth,
+        "loss_smoothness": loss_smoothness,
+        "loss": loss,
+        "image01": image01,
+        "image02": image02,
+    }
+
+    return loss, loss_info
