aa31418de8ae40f8d4064c35a0ad1ccc0f56751d
kirilllzaitsev <kzaitsev88@gmail.com>
2023-07-02 12:39:42 +0200

    ref: benchmarking setup

--- benchmarking/scripts/test_penet.sh
+++ benchmarking/scripts/test_penet.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-cd /media/master/wext/msc_studies/second_semester/research_project/related_work/PENet_ICRA2021 || exit 1
+cd /path/to/PENet_ICRA2021 || exit 1
 
-mkdir -p results/custom
+result_dir=results/custom
 
-python main.py -b 1 -n pe --evaluate models/pe.pth.tar --data-folder-save results/custom --test --cpu --save_prediction
+mkdir -p $result_dir
+
+python main.py -b 1 -n pe --evaluate models/pe.pth.tar --data-folder-save $result_dir --test --cpu --save_prediction --dataset-paths-dir /path/to/test/paths
